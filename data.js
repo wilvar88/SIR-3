774a1b43616dc0e448b83c848d4affbb1a088100
wilvar88 <138652332+wilvar88@users.noreply.github.com>
2025-08-15 02:54:00 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+// data.js — puntos de ejemplo. Edita/añade los tuyos.
+// x,y en PORCENTAJE (0–100) respecto a la imagen.
+window.HOTSPOTS = [
+  { x: 30.1, y: 23.4, n: 1,  title: "Código:",         text: "Código alfanumérico de 5 caracteres con el que se identificara la agencia."},
+  { x: 30.1, y: 29.4, n: 2,  title: "Nombre:",         text: "Nombre de la agencia a registrar. "},
+  { x: 30.1, y: 34.4, n: 3,  title: "Descripción:",    text: "Describir brevemente la agencia que se esta registrando." },
+  { x: 30.1, y: 38.9, n: 4,  title: "Empresa:",        text: "Lista desplegable donde se selecciona la empresa a la cual pertenece la empresa (Si la empresa es una op. De recaudo, en el formulario se despliega una serie de campos adicionales)." },
+  { x: 30.1, y: 43.8, n: 5,  title: "Estado:",         text: "Se selecciona de la lista desplegable el estado con que iniciara le agencia (Disponible, ocupado, fuera de servicio)." },
+  { x: 30.1, y: 50.6, n: 7,  title: "Dirección:",      text: "Dirección de la agencia de recaudo." },
+  { x: 30.1, y: 55.3, n: 8,  title: "Latitud:",        text: "Coordenadas en este caso la latitud." },
+  { x: 30.1, y: 59.9, n: 9,  title: "Longitud:",       text: "Coordenadas en este caso la longitud." },
+  { x: 30.1, y: 64.3, n: 10,  title: "Teléfono:",      text: "Teléfono de contacto de la agencia de recaudo." },
+  { x: 30.1, y: 69.0, n: 11, title: "Email:",          text: "Correo electrónico de contacto de la agencia de recaudo." },
+  { x: 30.1, y: 73.6, n: 12, title: "Website:",        text: "Sitio web de la agencia de recaudo, es un campo opcional." },
+  { x: 30.1, y: 79.1, n: 13, title: "Horario:",        text: "Campo de texto en el cual se indica el horario de atención de la agencia." },
+  { x: 30.1, y: 86.0, n: 14, title: "Servicios:",      text: "Campo de texto en el cual se describen los servicios de la agencia de recaudo." },
+  { x: 18.5, y: 90.5, n: 15, title: "Crear Agencia",   text: "Confirma la creación de la agencia" },
+  { x: 25.5, y: 90.5, n: 16, title: "Cancelar",        text: "Cancela el formulario" },
+  { x: 9.5, y: 46.5, n: 6,  title: "Checkbox agencia de recaudo:",  text: "Marca a la agencia como una agencia de recaudo." },
+];
+
